d676d793b305cf2db54e9c70748553cd8aac34db
AllenDodul <109014717+AllenDodul@users.noreply.github.com>
2022-12-26 21:10:41 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-fjarsd
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-fjarsd)
---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "./style.css";
+
+const todos = [
+  {
+    id: 1,
+    title: "Breakfast",
+    desc: "Finish breakfast at 9:00..." 
+  },
+  {
+    id: 2,
+    title: "Join Meeting",
+    desc: "Join Meeting at 10:00..." 
+  }
+];
+
+export default function App() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
