dd0d4cea1f30c72b1412a24ca50521c1eb0945dc
AllenDodul <109014717+AllenDodul@users.noreply.github.com>
2022-12-27 23:50:40 +0600

    1 file modified

--- src/NewTodo.jsx
+++ src/NewTodo.jsx
@@ -1,15 +1,30 @@
-import React from "react";
+import React,{useState} from "react";
 import style from "./newTodo.module.css";
 
 const NewTodo = () =>{
+  const [todo, setTodo] = useState({
+    title: '',
+    desc: ''
+  });
+  
+  const {title, desc} = todo;
+  const handleTodo = (e) =>{
+
+  }
   return(
     <form className={style.form}>
       <div className={style.inputBox}>
         <input 
           className={style.inputFild}
           type="text"
+          value={title}
+          onChange={handleTodo}
         />
-        <textarea className={style.inputFild}/>
+        <textarea 
+          className={style.inputFild}
+          value={desc}
+          onChange={handleTodo}
+        />
       </div>
       <div>
         <button className={style.btn}>Add Todo</button>
